d6457ff05bf20d68432d2c74359afb7b00a0f0dd
UnknownCode Persistent <matiaz.orellana@gmail.com>
2021-10-28 12:37:48 -0300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DepositFundsComponent } from './deposit-funds/deposit-funds.component';
+import { ManageAccountComponent } from './manage-account/manage-account.component';
+import { OpenAccountComponent } from './open-account/open-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DepositFundsComponent,
+    ManageAccountComponent,
+    OpenAccountComponent
   ],
   imports: [
     BrowserModule,
